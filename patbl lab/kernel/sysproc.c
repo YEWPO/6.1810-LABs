a9db260f51db24c8bfbdea0989b6b096c44cadf8
YEWPO <2021090908013@std.uestc.edu.cn>
2022-10-29 14:39:07 +0800

    Add files via upload

---
+++ patbl lab/kernel/sysproc.c
@@ -0,0 +1,130 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  argint(0, &n);
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  uint64 va;
+  int sz;
+  uint64 user_bitmask_addr;
+  int bitmask = 0;
+  struct proc * p = myproc();
+
+  argaddr(0, &va);
+  argint(1, &sz);
+  argaddr(2, &user_bitmask_addr);
+
+  if (sz > 32) {
+      return -1;
+  }
+
+  for (int i = 0; i < sz; ++i) {
+      pte_t * pte = walk(p->pagetable, va, 0);
+
+      if ((*pte) & PTE_A) {
+          bitmask |= (1 << i);
+          (*pte) ^= PTE_A;
+      }
+
+      va += PGSIZE;
+  }
+
+  if (copyout(p->pagetable, user_bitmask_addr, (char *)&bitmask, sizeof(bitmask)) < 0) {
+      return -1;
+  }
+
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
